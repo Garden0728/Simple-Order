73bf427f7fad05cf1d603f5f044c0d1b2dc3b28a
choi jung won <chlwjddnjs56@gmail.com>
2025-09-19 23:45:03 +0900

    feat(discount): change discount policy to rate-based
    - Switched from fixed discount to Rate discount policy
    - Currently implemented in pure Java
    - Will migrate to Spring DI (AppConfig) in the next step

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,13 +12,21 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 
 import java.lang.reflect.Member;
-
+//DI Container
 public class AppConfig { //기획자 같은 역할 동작 전체 구성 책임
-        //생성자 주입
+        //생성자 주입 //역학을 세우고 구현이 들어가는 설계
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
+
+    private static MemberRepository memberRepository() { //중복제거 하기 위해서 따로 추출
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
+    }
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
