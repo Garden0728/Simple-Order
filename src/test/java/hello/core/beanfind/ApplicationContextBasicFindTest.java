18e31eb4071cf78e3d0d340bb942ce2917b5ab98
choi jung won <chlwjddnjs56@gmail.com>
2025-09-21 20:44:37 +0900

    test: add bean lookup examples for ApplicationContext

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,47 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회") //구체적으로 적는건 좋지 않다, 항상 역할과 구현을 구분 그리고 역할에 의존해야한다
+    void findBeanByName2() {
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X") //ac.getBean("XXXXX", MemberServiceImpl.class))이 로직을 실횅하면  assertThrows(NoSuchBeanDefinitionException.class 예외가 터져야한다.
+    void findBeanByNameX(){
+        //ac.getBean("XXXXX", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                ()->ac.getBean("XXXXX", MemberServiceImpl.class));
+    }
+
+}
