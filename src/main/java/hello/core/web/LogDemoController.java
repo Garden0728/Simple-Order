73044326abdff030d992559a60b1190b4c2979a3
choi jung won <chlwjddnjs56@gmail.com>
2025-10-02 23:57:18 +0900

    test: verify bean scopes and Provider usage - singleton: created once at startup, managed until context shutdown - prototype: new instance per request, container stops managing after injection - request scope: new bean per HTTP request, destroyed at request end (@PreDestroy verified) - used Provider/ObjectProvider to correctly fetch scoped beans in singleton

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,31 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) throws InterruptedException { //request 정보 고객 요청 정보를 받을 수 있다.
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        Thread.sleep(1000);
+        logDemoService.logic("testId");
+        return "ok";
+    }
+
+}
--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,11 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -31,17 +33,18 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     public void disconnect() {
         System.out.println("close: " + url);
     }
-
-    @Override //의존관계 주입이 끝나면 호출해 주겠다
-    public void afterPropertiesSet() throws Exception {
+    @PostConstruct
+    //의존관계 주입이 끝나면 호출해 주겠다
+    public void init (){
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
-        System.out.println("NetworkClient.afterPropertiesSet");
+
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
